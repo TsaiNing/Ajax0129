b59f48b552b7ead4981732e4e041a4acc2b91462
Sherry <sherrysheep3743@gmail.com>
2024-01-29 17:07:50 +0800

    加入專案檔案。

---
+++ Ajax/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Ajax.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ajax.Controllers
+{
+    public class ApiController : Controller
+    {
+
+        private readonly MyDBContext _context;
+        public ApiController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _context.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+
+    }
+}
