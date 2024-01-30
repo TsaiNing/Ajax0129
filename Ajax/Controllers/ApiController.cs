f6b0a7b6f14d12eae6317163bef43c2b425d854b
Sherry <sherrysheep3743@gmail.com>
2024-01-30 10:45:35 +0800

    作業修改

--- Ajax/Controllers/ApiController.cs
+++ Ajax/Controllers/ApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Ajax.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace Ajax.Controllers
 {
@@ -14,9 +15,23 @@ namespace Ajax.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            //return View();
+            return Content("<h2>哈囉~Index</h2>", "text/html", Encoding.UTF8);
         }
 
+        public IActionResult Avatar(int id=1)
+        {
+            Member? member = _context.Members.Find(id);
+            if (member != null)
+            {
+                byte[] img = member.FileData;
+                if(img != null) 
+                {
+                    return File(img, "image/jpeg");
+                }
+            }
+            return NotFound();
+        }
         public IActionResult Cities()
         {
             var cities = _context.Addresses.Select(a => a.City).Distinct();
